9fdb66e33a6ef9d6fb17d19c24998a527f734332
EC2 Default User <ec2-user@ip-172-31-17-177.ec2.internal>
2022-01-31 07:54:41 +0000

    4-18

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy 
   has_one_attached :profile_image
 
   def get_profile_image(size)
@@ -13,5 +14,5 @@ class User < ApplicationRecord
     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
   profile_image.variant(resize: size).processed
- end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
- Rails.application.routes.draw do
+Rails.application.routes.draw do
   devise_for :users
-  root to: 'homes#top'
-  get "homes/about" => "homes#about", as: "about"
-  resources :post_images, only: [:new, :index, :show]
+  root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]  end
   resources :users, only: [:show, :edit, :update]
 end
